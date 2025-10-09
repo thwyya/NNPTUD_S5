2716d9aa1ae366150863b6fc5d4304887db239aa
TUNGNT <nguyenthanhtunnghutechsg@gmail.com>
2025-10-09 08:50:28 +0700

    cookie

--- routes/auth.js
+++ routes/auth.js
@@ -6,87 +6,109 @@ let bcrypt = require('bcrypt');
 let jwt = require('jsonwebtoken')
 
 
-router.post('/register', async function(req, res, next) {
-  let role = await roles.findOne({name:"USER"});
+
+router.post('/register', async function (req, res, next) {
+  let role = await roles.findOne({ name: "USER" });
   role = role._id;
   let newUser = new users({
-    username:req.body.username,
-    email:req.body.email,
-    password:req.body.password,
-    role:role
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+    role: role
   })
   await newUser.save();
   res.send({
-      success:true,
-      data:"dang ki thanh cong"
-    })
+    success: true,
+    data: "dang ki thanh cong"
+  })
 });
-router.post('/login', async function(req, res, next) {
+router.post('/login', async function (req, res, next) {
   let username = req.body.username;
   let password = req.body.password;
   let user = await users.findOne({
-    username:username
+    username: username
   })
-  if(user.length==0){
+  if (user.length == 0) {
     res.status(404).send({
-      success:true,
-      data:"user khong ton tai"
+      success: true,
+      data: "user khong ton tai"
     })
     return;
-  }else{
-    let result = bcrypt.compareSync(password,user.password);
-    if(result){
-    res.send({
-      success:true,
-      data:jwt.sign({
-        _id:user._id,
-        exp:Date.now()+15*60*1000
-      },"NNPTUD")
-    })
-    }else{
+  } else {
+    let result = bcrypt.compareSync(password, user.password);
+    if (result) {
+      let token = jwt.sign({
+        _id: user._id,
+        exp: Date.now() + 15 * 60 * 1000
+      }, "NNPTUD");
+      res.cookie("token", "Bearer " + token, {
+        httpOnly: true,
+        maxAge: 60 * 1000 * 60 * 24 * 7
+      })
       res.send({
-      success:true,
-      data:"user sai password"
-    })
+        success: true,
+        data: token
+      })
+    } else {
+      res.send({
+        success: true,
+        data: "user sai password"
+      })
     }
   }
 });
-
-router.get('/me',async function(req, res, next){
-  let token = req.headers.authorization;
-  if(token.startsWith("Bearer")){
-    token = token.split(" ")[1];
-    if(jwt.verify(token,"NNPTUD")){
-      if(jwt.decode(token).exp<Date.now()){
-        res.status(403).send({
-          success:false,
-          data:"user chua dang nhap 1"
-        })
-      }else{
-        let userId = jwt.decode(token)._id;
-        let user = await users.findById(userId).populate({
-    path: 'role',
-    select:'name'
-  });
-        if(user){
-            res.status(200).send({
-              success:true,
-              data:user
-            })
-        }
-      }
-    }else{
-      res.status(403).send({
-        success:false,
-        data:"user chua dang nhap 2"
-      })
-    }
-  }else{
-    res.status(403).send({
-      success:false,
-      data:"user chua dang nhap 3"
+router.post("/logout", function (req, res, next) {
+  try {
+    res.cookie("token","");
+    res.send({
+      success: true,
+      data: "Logout thanh cong"
+    })
+  } catch (error) {
+    res.send({
+      success: true,
+      data: error
     })
   }
 })
+router.get('/me', async function (req, res, next) {
+  let token = req.headers.authorization ? req.headers.authorization : req.cookies.token;
+  if (token && token.startsWith("Bearer")) {
+    token = token.split(" ")[1];
+    if (jwt.verify(token, "NNPTUD")) {
+      if (jwt.decode(token).exp < Date.now()) {
+        res.status(403).send({
+          success: false,
+          data: "user chua dang nhap"
+        })
+      } else {
+        let userId = jwt.decode(token)._id;
+        let user = await users.findById(userId).select(
+          "username avatarURL email fullname role"
+        ).populate({
+          path: 'role',
+          select: 'name'
+        });
+        if (user) {
+          res.status(200).send({
+            success: true,
+            data: user
+          })
+        }
+      }
+    } else {
+      res.status(403).send({
+        success: false,
+        data: "user chua dang nhap"
+      })
+    }
+  } else {
+    res.status(403).send({
+      success: false,
+      data: "user chua dang nhap"
+    })
+  }
+})
+
 
 module.exports = router;
