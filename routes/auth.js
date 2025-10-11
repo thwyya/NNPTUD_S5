3a65f2576a74ae5273c2924939d2b4ab06eebf5e
TUNGNT <nguyenthanhtunnghutechsg@gmail.com>
2025-10-11 08:25:04 +0700

    change password and split file

--- routes/auth.js
+++ routes/auth.js
@@ -5,22 +5,22 @@ let roles = require('../schemas/roles');
 let bcrypt = require('bcrypt');
 let jwt = require('jsonwebtoken')
 let { Response } = require('../utils/responseHandler')
-let {Authentication,Authorization}= require('../utils/authHandler')
+let { Authentication, Authorization } = require('../utils/authHandler')
+let { validatorLogin,validatorChangpassword,validatedResult } = require('../utils/validator')
 
 
-
-router.post('/register', async function (req, res, next) {
-  let role = await roles.findOne({ name: "USER" });
-  role = role._id;
-  let newUser = new users({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-    role: role
-  })
-  await newUser.save();
-  Response(res,200,true,"dang ki thanh cong");
-});
+router.post('/register', validatorLogin, validatedResult, async function (req, res, next) {
+    let role = await roles.findOne({ name: "USER" });
+    role = role._id;
+    let newUser = new users({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+      role: role
+    })
+    await newUser.save();
+    Response(res, 200, true, "dang ki thanh cong");
+  });
 router.post('/login', async function (req, res, next) {
   let username = req.body.username;
   let password = req.body.password;
@@ -28,7 +28,7 @@ router.post('/login', async function (req, res, next) {
     username: username
   })
   if (user.length == 0) {
-    Response(res,404,false,"user khong ton tai");
+    Response(res, 404, false, "user khong ton tai");
     return;
   } else {
     let result = bcrypt.compareSync(password, user.password);
@@ -41,7 +41,7 @@ router.post('/login', async function (req, res, next) {
         httpOnly: true,
         maxAge: 60 * 1000 * 60 * 24 * 7
       })
-      Response(res,200,true,token);
+      Response(res, 200, true, token);
     } else {
       Response(res, 403, false, "user sai password");
     }
@@ -55,14 +55,25 @@ router.post("/logout", function (req, res, next) {
     Response(res, 404, false, "token sai");
   }
 })
-router.get('/me',Authentication, Authorization("ADMIN","MOD","USER"),async function (req, res, next) {
-   let user = await users.findById(req.userId).select(
+router.get('/me', Authentication, Authorization("ADMIN", "MOD", "USER"), async function (req, res, next) {
+  let user = await users.findById(req.userId).select(
     "username email fullname avatarURL"
-   ).populate({
+  ).populate({
     path: 'role',
-    select:'name'
+    select: 'name'
   });
-   Response(res,200,true,user)
+  Response(res, 200, true, user)
+})
+router.post('/changepassword',Authentication,validatorChangpassword,validatedResult,async function (req,res,next) {
+    let user = await users.findById(req.userId);
+    if(bcrypt.compareSync(req.body.oldpassword,user.password)){
+      user.password = req.body.newpassword;
+      await user.save();
+      Response(res,200,true,"doi password thanh cong");
+    }else{
+      Response(res,400,false,"oldpassword khong dung");
+    }
+    
 })
 
 
