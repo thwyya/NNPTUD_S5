bb5bc4e7bf226af011372c780405cb8587efa39c
TUNGNT <nguyenthanhtunnghutechsg@gmail.com>
2025-10-22 09:52:42 +0700

    refactor upload file

--- routes/files.js
+++ routes/files.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var path = require('path')
 let fs = require('fs')
-let multer = require('multer');
+let { uploadAFileWithField, uploadMultiFilesWithField } = require('../utils/uploadHandler')
 const { Response } = require('../utils/responseHandler');
 
 router.get('/:filename', function (req, res, next) {
@@ -14,40 +14,16 @@ router.get('/:filename', function (req, res, next) {
     }
 })
 
-let Storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        if (file.mimetype.startsWith("image")) {
-            let pathFileStorage = path.join(__dirname, "../resources/images");
-            cb(null, pathFileStorage);
-        } else {
-            let pathFileStorage = path.join(__dirname, "../resources/files");
-            cb(null, pathFileStorage);
-        }
 
-    },
-    filename: function (req, file, cb) {
-        let extensionName = path.extname(file.originalname)
-        cb(null, Date.now() + extensionName);
-    }
-})
-let upload = multer({
-    storage: Storage,
-    fileFilter:function(req,file,cb){
-        console.log(file.mimetype);
-        if(!file.mimetype.startsWith("image")&&!file.mimetype.startsWith("application/pdf")){
-            cb(null,false)
-        }else{
-            cb(null,true)
-        }
-    },
-    limits: {
-        fileSize: 5 * 1024 * 1024
-    }
-})
-router.post("/uploads", upload.single('image'), function (req, res, next) {
+router.post("/uploads", uploadAFileWithField('image'), function (req, res, next) {
     let URL = `${req.protocol}://${req.get('host')}/files/${req.file.filename}`
     Response(res, 200, true, URL)
 })
-
+router.post("/uploadMulti", uploadMultiFilesWithField('image'), function (req, res, next) {
+    let URLs = req.files.map(function(file){
+        return `${req.protocol}://${req.get('host')}/files/${file.filename}`
+    })
+    Response(res, 200, true, URLs)
+})
 
 module.exports = router;
